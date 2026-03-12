49991af4f4a453010c1b4120dd92061f0a117451
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-12 13:00:30 +0100

    part4

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7166533fe99805b3a61376213582192732d33969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb099fa3478e94c03adf598bb54d3f2d902aefc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
