9ca3c79a668c68904a7190f1e8eb20cf3b1974eb
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-12 13:55:26 +0100

    update

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -34,7 +34,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Movies}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
